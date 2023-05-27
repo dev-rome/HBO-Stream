4cee6d5873561adf0a44a88eb35b268d3a17b933
dev-rome <haynesjerome@gmail.com>
2023-05-27 15:30:03 -0400

    created a ui folder and moved header and feature inside

---
+++ src/components/ui/FeaturedMedia.tsx
@@ -0,0 +1,36 @@
+import { FaPlay } from "react-icons/fa";
+
+const FeaturedMedia = () => {
+  return (
+    <div className="relative w-full h-screen min-h-[700px] bg-black overflow-hidden ">
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/573GCxqkYEg?autoplay=1&mute=1&loop=1"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+        className="absolute z-[1] scale-150"
+      ></iframe>
+      <div className="absolute w-full h-full z-[2] bg-background-movie flex items-end px-12 opacity-0 transition-opacity duration-500 ease-in-out hover:opacity-100">
+        <div className="">
+          <h2 className="text-6xl text-color-secondary font-bold mb-4">The Movie</h2>
+          <p className="text-sm text-color-secondary font-bold mb-2">Now Playing.</p>
+          <p className="text-sm text-color-secondary font-bold mb-2">
+            In theaters now and on HBO MAX.
+          </p>
+          <div className="flex items-center gap-3 mb-20">
+            <button className="bg-color-secondary w-10 h-10 rounded-[50%] flex justify-center items-center transform transition duration-300 ease-in-out hover:scale-110">
+              <FaPlay />
+            </button>
+            <button className="bg-color-tertiary flex justify-center items-center w-24 h-10 rounded-3xl text-xs text-color-secondary uppercase font-bold transform transition duration-300 ease-in-out hover:scale-110">
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedMedia;
---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { HiOutlineSearch } from "react-icons/hi";
+import { HiOutlineBars3CenterLeft } from "react-icons/hi2";
+import Image from "next/image";
+import useMenuContext from "@/src/hooks/useMenuContext";
+
+const Header = () => {
+  // Extracting toggleSideNav values from the menu context
+  const { toggleSideNav } = useMenuContext() || {};
+  // Invokes the toggleSideNav function if it exists or true
+  const handleOpenMenuClick = () => {
+    toggleSideNav?.();
+  };
+
+  return (
+    <header className="fixed top-0 left-0 z-10 w-full flex justify-between items-center text-color-secondary bg-transparent transition-colors duration-300 ease-in-out hover:bg-[#212529] py-4 px-12">
+      <div className="flex gap-10">
+        <HiOutlineBars3CenterLeft
+          className="cursor-pointer"
+          onClick={handleOpenMenuClick}
+        />
+        <HiOutlineSearch className="icon" />
+      </div>
+      <Image
+        src="/assets/images/logo.svg"
+        alt="HBO Max"
+        width={130}
+        height={130}
+      />
+      <div className="flex items-center gap-4">
+        <Image
+          src="/assets/images/pic.jpeg"
+          alt="User account image"
+          width={30}
+          height={30}
+          className="rounded-full"
+        />
+        <p>Jerome</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
