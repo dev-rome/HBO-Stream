54b7b9104559f03618afd1f6650529816dfc19cb
dev-rome <haynesjerome@gmail.com>
2023-05-28 17:54:03 -0400

    added account side nav toggle

--- src/context/menu.tsx
+++ src/context/menu.tsx
@@ -4,7 +4,9 @@ import { createContext, useState } from "react";
 
 interface MenuContextProps {
   showSideNav: boolean;
+  showAccountSideNav: boolean;
   toggleSideNav: () => void;
+  toggleAccountSideNav: () => void;
 }
 
 interface MenuProviderProps {
@@ -13,20 +15,34 @@ interface MenuProviderProps {
 // Defines a default value for the menu context
 const defaultMenuContext: MenuContextProps = {
   showSideNav: false, // Default value for showing side navigation
+  showAccountSideNav: false,
   toggleSideNav: () => {}, // Default placeholder function for toggling side navigation
+  toggleAccountSideNav: () => {},
 };
 
 const MenuContext = createContext<MenuContextProps>(defaultMenuContext);
 
 const MenuProvider = ({ children }: MenuProviderProps) => {
   const [showSideNav, setShowSideNav] = useState<boolean>(false);
+  const [showAccountSideNav, setShowAccountSideNav] = useState<boolean>(false);
 
   const toggleSideNav = () => {
     setShowSideNav((prevValue) => !prevValue);
   };
 
+  const toggleAccountSideNav = () => {
+    setShowAccountSideNav((prevValue) => !prevValue);
+  };
+
   return (
-    <MenuContext.Provider value={{ showSideNav, toggleSideNav }}>
+    <MenuContext.Provider
+      value={{
+        showSideNav,
+        showAccountSideNav,
+        toggleSideNav,
+        toggleAccountSideNav,
+      }}
+    >
       {children}
     </MenuContext.Provider>
   );
