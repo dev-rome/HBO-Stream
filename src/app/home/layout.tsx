0cc03b8e76abe9fbb02da4d18c673612709d86bc
dev-rome <haynesjerome@gmail.com>
2023-05-28 18:15:59 -0400

    imported just added and poster view component and removed side nav and account component

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,11 +1,9 @@
 import Header from "@/src/components/ui/Header";
-import SideNavMenu from "@/src/components/side-nav/SideNavMenu";
 
 function HomePageLayout({ children }: { children: React.ReactNode }) {
   return (
     <section className="min-h-screen bg-color-primary">
       <Header />
-      <SideNavMenu />
       {children}
     </section>
   );
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,15 @@
 import FeaturedMedia from "@/src/components/ui/FeaturedMedia";
 import ForYouList from "@/src/components/ui/ForYouList";
+import JustAdded from "@/src/components/ui/JustAdded";
+import PosterView from "@/src/components/ui/PosterView";
 
 function HomePage() {
   return (
     <>
       <FeaturedMedia />
       <ForYouList />
+      <JustAdded />
+      <PosterView />
     </>
   );
 }
