d18f60cb212f339b817445bffe1c45f543ff1a82
dev-rome <haynesjerome@gmail.com>
2023-05-28 18:43:14 -0400

    added toggle account side nav to close menu when nav item is clicked

---
+++ src/components/side-nav/AccountSideNav.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useState } from "react";
+import useMenuContext from "@/src/hooks/useMenuContext";
+import NavItem from "@/src/components/side-nav/NavItem";
+
+const AccountSideNav = () => {
+  const [activeItem, setActiveItem] = useState<string>("My List");
+
+  const { toggleAccountSideNav } = useMenuContext() || {};
+
+  const handleActiveItemClick = (item: string) => {
+    setActiveItem(item);
+    toggleAccountSideNav?.();
+  };
+
+  const accountNavTop = [
+    { title: "My List", href: "/" },
+    { title: "Continue Watching", href: "" },
+  ];
+
+  const accountNavMiddle = [
+    { title: "Account", href: "" },
+    { title: "Billing Information", href: "" },
+    { title: "Manage Devices", href: "" },
+    { title: "Parental Controls", href: "" },
+    { title: "Notifications", href: "" },
+  ];
+
+  const accountNavBottom = [
+    { title: "Switch Profiles", href: "" },
+    { title: "Help", href: "" },
+    { title: "Sign Out", href: "" },
+  ];
+
+  return (
+    <nav className="bg-color-primary p-11">
+      <ul>
+        {accountNavTop.map(({ title, href }) => (
+          <NavItem
+            key={title}
+            title={title}
+            href={href}
+            activeItem={activeItem === title}
+            onClick={() => handleActiveItemClick(title)}
+          />
+        ))}
+      </ul>
+      <hr className="border-t-2 border-gray-200 my-5" />
+      <ul>
+        {accountNavMiddle.map(({ title, href }) => (
+          <NavItem
+            key={title}
+            title={title}
+            href={href}
+            activeItem={activeItem === title}
+            onClick={() => handleActiveItemClick(title)}
+          />
+        ))}
+      </ul>
+      <hr className="border-t-2 border-gray-200 my-5" />
+      <ul>
+        {accountNavBottom.map(({ title, href }) => (
+          <NavItem
+            key={title}
+            title={title}
+            href={href}
+            activeItem={activeItem === title}
+            onClick={() => handleActiveItemClick(title)}
+          />
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default AccountSideNav;
