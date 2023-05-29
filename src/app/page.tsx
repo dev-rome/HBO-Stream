8d665d8af4bc39338189f29a116a5bfe14368fc3
dev-rome <haynesjerome@gmail.com>
2023-05-29 15:48:36 -0400

    change home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,80 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 export default function Home() {
-  const [activeButton, setActiveButton] = useState<string>("kid");
+  const [activeButton, setActiveButton] = useState<string>("sign-up");
 
-  // handleMouseEnter is a function that sets the active button based on the button the mouse has entered
-  // Possible values for buttonName are 'adult' and 'kid'.
   const handleMouseEnter = (buttonName: string) => {
     setActiveButton(buttonName);
   };
 
+  const variants = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+  };
+
   return (
-    <section className="bg-background-home">
-      <div className="flex flex-col justify-between items-center h-screen">
-        <div className="flex flex-col content-center items-center gap-4 mt-8">
-          <Image
-            src="/assets/images/logo.svg"
-            width={130}
-            height={130}
-            alt="HBO Max"
-          />
-          <span className="text-2xl font-light text-color-secondary">
-            Who is Watching?
-          </span>
-        </div>
-        <div className="flex justify-center items-center">
-          <div className="flex flex-col justify-center items-center gap-x-12">
-            <Image
-              src="/assets/images/pic.jpeg"
-              alt="profile-image"
-              width={125}
-              height={125}
-              className="rounded-full object-cover mb-4 cursor-pointer"
-            />
-            <p className="text-color-secondary">Jerome</p>
-          </div>
-        </div>
-        <div className="flex gap-5 mb-8">
-          <button
-            onMouseEnter={() => handleMouseEnter("adult")}
-            onFocus={() => handleMouseEnter("adult")}
-            className={`bg-zinc-50/[.2] text-color-secondary uppercase font-bold rounded-2xl w-32 h-9 ${
-              activeButton === "adult" ? "opacity-100" : "opacity-40"
-            }`}
-          >
-            Add Adult
-          </button>
-          <button
-            onMouseEnter={() => handleMouseEnter("kid")}
-            onFocus={() => handleMouseEnter("kid")}
-            className={`bg-zinc-50/[.2] text-color-secondary font-bold uppercase rounded-2xl w-32 h-9 ${
-              activeButton === "kid" ? "opacity-100" : "opacity-40"
-            }`}
-          >
-            Add Kid
-          </button>
-        </div>
+    <motion.section
+      className="relative bg-background-main bg-center bg-cover bg-no-repeat h-screen"
+      variants={variants}
+      initial="initial"
+      animate="animate"
+      transition={{ duration: 0.5, ease: "easeIn" }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50 w-full h-full"></div>
+      <div className="absolute z-10 top-1/4 pl-4 lg:pl-12">
+        <motion.h1
+          className="max-w-2xl text-6xl leading-tight text-color-secondary font-bold mb-2"
+          variants={variants}
+          initial="initial"
+          animate="animate"
+          transition={{ duration: 0.5, ease: "easeIn", delay: 0.7 }}
+        >
+          Unlimited Movies, TV Shows, and More!
+        </motion.h1>
+        <motion.p
+          className="text-2xl text-color-secondary mb-5"
+          variants={variants}
+          initial="initial"
+          animate="animate"
+          transition={{ duration: 0.5, ease: "easeIn", delay: 1.4 }}
+        >
+          Watch anywhere. Cancel anytime.
+        </motion.p>
+        <motion.div
+          className="flex gap-5"
+          variants={variants}
+          initial="initial"
+          animate="animate"
+          transition={{ duration: 0.5, ease: "easeIn", delay: 2.1 }}
+        >
+          <Link href="/login">
+            <button
+              onMouseEnter={() => handleMouseEnter("sign-in")}
+              onFocus={() => handleMouseEnter("sign-in")}
+              className={`bg-color-secondary text-color-primary font-semibold rounded-2xl w-32 h-9 ${
+                activeButton === "sign-in" ? "opacity-100" : "opacity-40"
+              }`}
+            >
+              Sign-In
+            </button>
+          </Link>
+          <Link href="/create-user">
+            <button
+              onMouseEnter={() => handleMouseEnter("sign-up")}
+              onFocus={() => handleMouseEnter("sign-up")}
+              className={`bg-color-secondary text-color-primary font-semibold rounded-2xl w-32 h-9 ${
+                activeButton === "sign-up" ? "opacity-100" : "opacity-40"
+              }`}
+            >
+              Sign-Up
+            </button>
+          </Link>
+        </motion.div>
       </div>
-    </section>
+    </motion.section>
   );
 }
