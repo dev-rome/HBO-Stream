a6b29b0f060a93eb39e7b452bc244e58caa2f6ae
dev-rome <haynesjerome@gmail.com>
2023-05-30 14:14:26 -0400

    added check for href

--- src/components/ActiveButton.tsx
+++ src/components/ActiveButton.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
 interface ActiveButtonProps {
-  href: string;
+  href?: string;
   buttonName: string;
   activeButton: string;
   handleMouseEnter: (buttonName: string) => void;
   handleFocus: (buttonName: string) => void;
+  onClick?: () => void;
 }
 
 const ActiveButton = ({
@@ -14,20 +15,22 @@ const ActiveButton = ({
   activeButton,
   handleMouseEnter,
   handleFocus,
+  onClick,
 }: ActiveButtonProps) => {
-  return (
-    <Link href={href}>
-      <button
-        onMouseEnter={() => handleMouseEnter(buttonName)}
-        onFocus={() => handleFocus(buttonName)}
-        className={`bg-color-secondary text-color-primary font-semibold rounded-2xl w-32 h-9 ${
-          activeButton === buttonName ? "opacity-100" : "opacity-40"
-        }`}
-      >
-        {buttonName}
-      </button>
-    </Link>
+  const buttonContent = (
+    <button
+      onMouseEnter={() => handleMouseEnter(buttonName)}
+      onFocus={() => handleFocus(buttonName)}
+      onClick={onClick}
+      className={`bg-color-secondary text-color-primary font-semibold rounded-2xl w-32 h-9 ${
+        activeButton === buttonName ? "opacity-100" : "opacity-40"
+      }`}
+    >
+      {buttonName}
+    </button>
   );
+
+  return href ? <Link href={href}>{buttonContent}</Link> : buttonContent;
 };
 
 export default ActiveButton;
