a69c936bd7818c0a84e8cfa5da4fe86a2830f1f4
dev-rome <haynesjerome@gmail.com>
2023-05-30 19:48:34 -0400

    Deleted old section files

--- src/components/MediaRow.tsx
+++ src/components/MediaRow.tsx
@@ -27,13 +27,9 @@ const MediaRow = ({ title, imgWidth, imgHeight }: MediaRowProps) => {
         {imagesData.map((item, index) => (
           <div key={index} className="relative">
             <div
-              className={`w-[${imgWidth}] h-[${imgHeight}] overflow-hidden flex items-center justify-center1}`}
+              className={`w-[${imgWidth}] h-[${imgHeight} overflow-hidden flex items-center justify-center`}
             >
-              <img
-                className="w-full h-full object-cover"
-                src={item}
-                alt="Placeholder description"
-              />
+              <img src={item} alt="" className="w-full h-full object-cover" />
             </div>
             <div className="group absolute top-0 left-0 w-full h-full flex justify-center items-center bg-background-movie-poster opacity-0 transition-opacity duration-500 ease-in-out hover:opacity-90">
               <button className="transform translate-y-full opacity-0 transition-all duration-500 ease-in-out group-hover:translate-y-0 group-hover:opacity-100 hover:scale-125 text-white text-4xl">
