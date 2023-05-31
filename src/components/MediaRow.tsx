ee2ebcfd7407706bb2f95da487a2c46c13131d6e
dev-rome <haynesjerome@gmail.com>
2023-05-30 20:41:44 -0400

    fix image size

--- src/components/MediaRow.tsx
+++ src/components/MediaRow.tsx
@@ -24,12 +24,16 @@ const MediaRow = ({ title, imgWidth, imgHeight }: MediaRowProps) => {
     <div className="py-5 pl-4 lg:pl-12">
       <h3 className="text-color-secondary font-medium mb-4">{title}</h3>
       <div className="min-w-full w-calc-width flex flex-nowrap gap-3 overflow-x-scroll no-scrollbar">
-        {imagesData.map((item, index) => (
-          <div key={index} className="relative">
+        {imagesData.map((item) => (
+          <div key={title} className="relative">
             <div
-              className={`w-[${imgWidth}] h-[${imgHeight} overflow-hidden flex items-center justify-center`}
+              className={`overflow-hidden flex items-center justify-center w-[${imgWidth}] h-[${imgHeight}]`}
             >
-              <img src={item} alt="" className="w-full h-full object-cover" />
+              <img
+                className="w-full h-full object-cover"
+                src={item}
+                alt="Placeholder description"
+              />
             </div>
             <div className="group absolute top-0 left-0 w-full h-full flex justify-center items-center bg-background-movie-poster opacity-0 transition-opacity duration-500 ease-in-out hover:opacity-90">
               <button className="transform translate-y-full opacity-0 transition-all duration-500 ease-in-out group-hover:translate-y-0 group-hover:opacity-100 hover:scale-125 text-white text-4xl">
