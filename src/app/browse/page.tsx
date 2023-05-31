0bebeeef35a0ca6c9097e4cf75c4678cd6f4d658
dev-rome <haynesjerome@gmail.com>
2023-05-31 15:29:28 -0400

    edit browse file

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -9,16 +9,61 @@ function Browse() {
     <>
       <FeaturedMedia />
       <MediaRow title="For You" imgWidth="240px" imgHeight="360px" />
-      <MediaRow title="Series" genreId="10765" imgWidth="450px" imgHeight="255px" />
-      <MediaRow title="Action" genreId="28" imgWidth="240px" imgHeight="360px" />
-      <MediaRow title="Thriller" genreId="53" imgWidth="240px" imgHeight="360px" />
-      <MediaRow title="Comedy" genreId="35" imgWidth="240px" imgHeight="360px" />
-      <MediaRow title="Animation" genreId="16" imgWidth="600px" imgHeight="295px" />
-      <MediaRow title="Horror" genreId="27" imgWidth="240px" imgHeight="360px" />
-      <MediaRow title="Documentary" genreId="99" imgWidth="240px" imgHeight="360px" />
-      <MediaRow title="Romance" genreId="10749" imgWidth="700px" imgHeight="395px" />
+      <MediaRow
+        title="Series"
+        genreId="10765"
+        imgWidth="450px"
+        imgHeight="255px"
+      />
+      <MediaRow
+        title="Action"
+        genreId="28"
+        imgWidth="240px"
+        imgHeight="360px"
+      />
+      <MediaRow
+        title="Thriller"
+        genreId="53"
+        imgWidth="240px"
+        imgHeight="360px"
+      />
+      <MediaRow
+        title="Comedy"
+        genreId="35"
+        imgWidth="240px"
+        imgHeight="360px"
+      />
+      <MediaRow
+        title="Animation"
+        genreId="16"
+        imgWidth="600px"
+        imgHeight="295px"
+      />
+      <MediaRow
+        title="Horror"
+        genreId="27"
+        imgWidth="240px"
+        imgHeight="360px"
+      />
+      <MediaRow
+        title="Documentary"
+        genreId="99"
+        imgWidth="240px"
+        imgHeight="360px"
+      />
+      <MediaRow
+        title="Romance"
+        genreId="10749"
+        imgWidth="700px"
+        imgHeight="395px"
+      />
       <MediaRow title="Crime" genreId="80" imgWidth="240px" imgHeight="360px" />
-      <MediaRow title="Science Fiction" genreId="878" imgWidth="240px" imgHeight="360px" />
+      <MediaRow
+        title="Science Fiction"
+        genreId="878"
+        imgWidth="240px"
+        imgHeight="360px"
+      />
     </>
   );
 }
