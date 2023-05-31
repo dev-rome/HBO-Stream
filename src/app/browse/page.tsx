cb381d8acaefb76ab791f27fddbd262408c07104
dev-rome <haynesjerome@gmail.com>
2023-05-31 17:00:52 -0400

    added next dynamic to lazy load components

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -1,64 +1,71 @@
 "use client";
 
 import FeaturedMedia from "@/src/components/ui/FeaturedMedia";
-import MediaRow from "@/src/components/MediaRow";
 import AuthCheck from "@/src/features/AuthCheck";
+import dynamic from "next/dynamic";
+
+const DynamicMediaRow = dynamic(() => import("@/src/components/MediaRow"));
 
 function Browse() {
   return (
     <>
       <FeaturedMedia />
-      <MediaRow title="For You" imgWidth="240px" imgHeight="360px" />
-      <MediaRow
+      <DynamicMediaRow title="For You" imgWidth="240px" imgHeight="360px" />
+      <DynamicMediaRow
         title="Series"
         genreId="10765"
-        imgWidth="450px"
-        imgHeight="255px"
+        imgWidth="240px"
+        imgHeight="360px"
       />
-      <MediaRow
+      <DynamicMediaRow
         title="Action"
         genreId="28"
         imgWidth="240px"
         imgHeight="360px"
       />
-      <MediaRow
+      <DynamicMediaRow
         title="Thriller"
         genreId="53"
         imgWidth="240px"
         imgHeight="360px"
       />
-      <MediaRow
+      <DynamicMediaRow
         title="Comedy"
         genreId="35"
         imgWidth="240px"
         imgHeight="360px"
       />
-      <MediaRow
+      <DynamicMediaRow
         title="Animation"
         genreId="16"
-        imgWidth="600px"
-        imgHeight="295px"
+        imgWidth="240px"
+        imgHeight="360px"
       />
-      <MediaRow
+      <DynamicMediaRow
         title="Horror"
         genreId="27"
         imgWidth="240px"
         imgHeight="360px"
       />
-      <MediaRow
+      <DynamicMediaRow
         title="Documentary"
         genreId="99"
         imgWidth="240px"
         imgHeight="360px"
       />
-      <MediaRow
+      <DynamicMediaRow
         title="Romance"
         genreId="10749"
-        imgWidth="700px"
-        imgHeight="395px"
+        imgWidth="240px"
+        imgHeight="360px"
       />
-      <MediaRow title="Crime" genreId="80" imgWidth="240px" imgHeight="360px" />
-      <MediaRow
+      <DynamicMediaRow
+        title="Crime"
+        genreId="80"
+        imgWidth="240px"
+        imgHeight="360px"
+      />
+      <DynamicMediaRow
         title="Science Fiction"
         genreId="878"
         imgWidth="240px"
