d7f9c9760d2ca3324fee9549bb31774098f5b5fa
dev-rome <haynesjerome@gmail.com>
2023-05-31 16:15:08 -0400

    removed space in css file and removed the scroll class in tailwind

--- src/components/MediaRow.tsx
+++ src/components/MediaRow.tsx
@@ -64,7 +64,7 @@ const MediaRow = ({ title, imgWidth, imgHeight, genreId }: MediaRowProps) => {
   return (
     <div className="py-5 pl-4 lg:pl-12">
       <h3 className="text-color-secondary font-medium mb-4">{title}</h3>
-      <div className="min-w-full w-calc-width flex flex-nowrap gap-3 overflow-x-scroll no-scrollbar">
+      <div className="min-w-full w-calc-width flex flex-nowrap gap-3 no-scrollbar">
         {renderImages}
       </div>
     </div>
