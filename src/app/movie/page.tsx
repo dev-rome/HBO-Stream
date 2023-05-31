d6bd88e325514fbe9f9bb5abf610433b463668ff
dev-rome <haynesjerome@gmail.com>
2023-05-31 19:01:38 -0400

    added values to movie page

--- src/app/movie/page.tsx
+++ src/app/movie/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import FeaturedMedia from "@/src/components/ui/FeaturedMedia";
 import MediaRow from "@/src/components/MediaRow";
@@ -7,8 +7,17 @@ import CastInfo from "@/src/components/ui/cast-info/CastInfo";
 function Movie() {
   return (
     <>
-      <FeaturedMedia />
-      <MediaRow title="More like this" genreId="28"  imgWidth="240px" imgHeight="360px" />
+      <FeaturedMedia
+        title="The Movie"
+        location="In theaters now and on HBO Stream."
+        video="https://www.youtube.com/embed/573GCxqkYEg?autoplay=1&mute=1&loop=1"
+      />
+      <MediaRow
+        title="More like this"
+        genreId="28"
+        imgWidth="240px"
+        imgHeight="360px"
+      />
       <CastInfo />
     </>
   );
