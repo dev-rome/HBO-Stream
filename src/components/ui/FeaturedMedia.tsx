f994fc4477f9c1814957a956ae7c9d3e39599c76
dev-rome <haynesjerome@gmail.com>
2023-05-31 23:51:52 -0400

    added new type, change to overview, and added links

--- src/components/ui/FeaturedMedia.tsx
+++ src/components/ui/FeaturedMedia.tsx
@@ -1,16 +1,19 @@
 import { FaPlay } from "react-icons/fa";
+import Link from "next/link";
 
 interface FeaturedMediaProps {
   title: string;
-  location: string;
+  overview: string;
   video: string;
+  // movieId: string;
 }
 
-const FeaturedMedia = ({ title, location, video }: FeaturedMediaProps) => {
-  const handleButtonClick = () => {
-    console.log("Button Clicked");
-  };
-
+const FeaturedMedia = ({
+  title,
+  overview,
+  video,
+}: // movieId,
+FeaturedMediaProps) => {
   return (
     <div className="relative w-full h-screen min-h-[700px] bg-black overflow-hidden ">
       <iframe
@@ -22,30 +25,27 @@ const FeaturedMedia = ({ title, location, video }: FeaturedMediaProps) => {
         allowFullScreen
         className="absolute z-[1] scale-150"
       ></iframe>
-      <div className="absolute w-full h-full z-[2] bg-background-movie flex items-end px-12 opacity-0 transition-opacity duration-500 ease-in-out hover:opacity-100">
+      <div className="absolute w-full h-full z-[2] bg-background-movie flex items-end px-4 md:px-12 opacity-0 transition-opacity duration-500 ease-in-out hover:opacity-100">
         <div className="content-wrapper">
-          <h2 className="text-6xl text-color-secondary font-bold mb-4">
+          <h2 className="text-5xl md:text-6xl text-color-secondary font-bold mb-4">
             {title}
           </h2>
-          <p className="text-sm text-color-secondary font-bold mb-2">
-            Now Playing.
-          </p>
-          <p className="text-sm text-color-secondary font-bold mb-2">
-            {location}
+          <p className="max-w-2xl hidden md:block text-color-secondary font-bold mb-2">
+            {overview}
           </p>
           <div className="flex items-center gap-3 mb-20">
-            <button
+            <Link
+              href={``}
               className="bg-color-secondary w-10 h-10 rounded-[50%] flex justify-center items-center transform transition duration-300 ease-in-out hover:scale-110"
-              onClick={handleButtonClick}
             >
               <FaPlay />
-            </button>
-            <button
+            </Link>
+            <Link
+              href={``}
               className="bg-color-tertiary flex justify-center items-center w-24 h-10 rounded-3xl text-xs text-color-secondary uppercase font-bold transform transition duration-300 ease-in-out hover:scale-110"
-              onClick={handleButtonClick}
             >
               More Info
-            </button>
+            </Link>
           </div>
         </div>
       </div>
