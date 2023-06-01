7fc332e48a6257931c1e96699724e8bdf6d3f443
dev-rome <haynesjerome@gmail.com>
2023-06-01 16:02:03 -0400

    added cast info for movie

--- src/components/ui/cast-info/CastInfo.tsx
+++ src/components/ui/cast-info/CastInfo.tsx
@@ -1,22 +1,52 @@
-import CastInfoSection from "./CastInfoSection";
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+import CastInfoSection from "@/src/components/ui/cast-info/CastInfoSection";
+import axios from "axios";
+
+interface TeamData {
+  cast: { name: string }[];
+  crew: { name: string; known_for_department: string }[];
+}
 
 const CastInfo = () => {
-  const crewData = [
-    { role: "Name", name: "Name" },
-    { role: "Name", name: "Name" },
-    { role: "Name", name: "Name" },
-    { role: "Name", name: "Name" },
-  ];
+  const [teamData, setTeamData] = useState<TeamData>({
+    cast: [],
+    crew: [],
+  });
+  const params = useParams();
 
-  const directorData = [{ role: "Name", name: "Name" }];
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${params.id}/credits?api_key=${process.env.NEXT_PUBLIC_MOVIE_DB_API_KEY}`
+      )
+      .then((res) => {
+        setTeamData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [params.id]);
 
-  const writerData = [{ role: "Name", name: "Name" }];
+  const castData = teamData.cast.map((item) => {
+    return {
+      name: item.name,
+    };
+  });
+
+  const crewDataDirecting = teamData.crew
+    .filter((member) => member.known_for_department === "Directing")
+    .map((member) => ({ name: member.name }));
+
+  const crewDataWriting = teamData.crew
+    .filter((member) => member.known_for_department === "Writing")
+    .map((member) => ({ name: member.name }));
 
   return (
     <div className="flex flex-col gap-10 px-4 py-12 lg:px-12 text-color-secondary">
-      <CastInfoSection title="Cast & Crew" data={crewData} />
-      <CastInfoSection title="Director" data={directorData} />
-      <CastInfoSection title="Writers" data={writerData} />
+      <CastInfoSection title="Cast & Crew" data={castData} />
+      <CastInfoSection title="Directing" data={crewDataDirecting} />
+      <CastInfoSection title="Writing " data={crewDataWriting} />
     </div>
   );
 };
--- src/components/ui/cast-info/CastInfoSection.tsx
+++ src/components/ui/cast-info/CastInfoSection.tsx
@@ -1,21 +1,32 @@
 interface CastInfoSectionProps {
   title: string;
-  data: {
-    role: string;
-    name: string;
-  }[];
+  data: { name: string }[];
 }
 
 const CastInfoSection = ({ title, data }: CastInfoSectionProps) => {
+  const midpoint = Math.ceil(data.length / 2);
+  const leftColumnData = data.slice(0, midpoint);
+  const rightColumnData = data.slice(midpoint);
+
   return (
     <div>
       <h2 className="text-4xl font-bold mb-8">{title}</h2>
-      {data.map(({ role, name }, index) => (
-        <ul className="flex" key={index}>
-          <li className="mr-32 w-[6.25rem]">{role}</li>
-          <li>{name}</li>
+      <div className="flex">
+        <ul className="flex flex-col">
+          {leftColumnData.map(({ name }, index) => (
+            <li className="mb-2" key={index}>
+              {name}
+            </li>
+          ))}
         </ul>
-      ))}
+        <ul className="flex flex-col ml-8">
+          {rightColumnData.map(({ name }, index) => (
+            <li className="mb-2" key={index}>
+              {name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
