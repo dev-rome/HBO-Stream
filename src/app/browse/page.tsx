3e871a8b297bc14a9ab91337cb8afbabd6e8365b
dev-rome <haynesjerome@gmail.com>
2023-06-03 16:01:12 -0400

    added media type to dynamic media row

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -14,66 +14,76 @@ function Browse() {
         overview="In theaters now and on HBO Stream."
         video="https://www.youtube.com/embed/573GCxqkYEg?autoplay=1&mute=1&loop=1"
       />
-      <DynamicMediaRow title="For You" imgWidth="240px" imgHeight="360px" />
+      <DynamicMediaRow title="For You" imgWidth="240px" imgHeight="360px" media_type="movie" />
       <DynamicMediaRow
         title="Series"
         genreId="10765"
         imgWidth="240px"
         imgHeight="360px"
+        media_type="tv"
       />
       <DynamicMediaRow
         title="Action"
         genreId="28"
         imgWidth="240px"
         imgHeight="360px"
+        media_type="movie"
       />
       <DynamicMediaRow
         title="Thriller"
         genreId="53"
         imgWidth="240px"
         imgHeight="360px"
+        media_type="movie"
       />
       <DynamicMediaRow
         title="Comedy"
         genreId="35"
         imgWidth="240px"
         imgHeight="360px"
+        media_type="movie"
       />
       <DynamicMediaRow
         title="Animation"
         genreId="16"
         imgWidth="240px"
         imgHeight="360px"
+        media_type="movie"
       />
       <DynamicMediaRow
         title="Horror"
         genreId="27"
         imgWidth="240px"
         imgHeight="360px"
+        media_type="movie"
       />
       <DynamicMediaRow
         title="Documentary"
         genreId="99"
         imgWidth="240px"
         imgHeight="360px"
+        media_type="movie"
       />
       <DynamicMediaRow
         title="Romance"
         genreId="10749"
         imgWidth="240px"
         imgHeight="360px"
+        media_type="movie"
       />
       <DynamicMediaRow
         title="Crime"
         genreId="80"
         imgWidth="240px"
         imgHeight="360px"
+        media_type="movie"
       />
       <DynamicMediaRow
         title="Science Fiction"
         genreId="878"
         imgWidth="240px"
         imgHeight="360px"
+        media_type="movie"
       />
     </>
   );
