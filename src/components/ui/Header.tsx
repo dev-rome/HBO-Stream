399b0afc5cf529ae1677aa33f0a123a9faf596b3
dev-rome <haynesjerome@gmail.com>
2023-06-06 13:17:40 -0400

    change text size and gap size

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -27,8 +27,8 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed top-0 left-0 z-10 w-full flex justify-between items-center text-color-secondary bg-transparent transition-colors duration-300 ease-in-out hover:bg-[#212529] py-4 px-12">
-      <div className="flex gap-10">
+    <header className="fixed top-0 left-0 z-10 w-full flex justify-between items-center text-color-secondary bg-[#212529] py-4 px-12">
+      <div className="flex gap-5">
         <HiOutlineBars3CenterLeft
           className="text-xl cursor-pointer"
           onClick={handleOpenMenuClick}
@@ -51,11 +51,11 @@ const Header = () => {
           <Image
             src="/assets/images/pic.jpeg"
             alt="User account image"
-            width={30}
-            height={30}
+            width={25}
+            height={25}
             className="rounded-full"
           />
-          <p>Jerome</p>
+          <p className="text-sm">Jerome</p>
         </div>
       </button>
       <Account />
