e00d13c701d87fc9032830a035bebe13aad10488
dev-rome <haynesjerome@gmail.com>
2023-06-06 13:23:17 -0400

    removed buttons and functions

--- src/components/ui/FeaturedImage.tsx
+++ src/components/ui/FeaturedImage.tsx
@@ -1,4 +1,4 @@
-import { FaPlay, FaPlus } from "react-icons/fa";
+import { FaPlay } from "react-icons/fa";
 import Image from "next/image";
 
 interface FeaturedImageProps {
@@ -8,17 +8,13 @@ interface FeaturedImageProps {
 }
 
 const FeaturedImage = ({ title, overview, image }: FeaturedImageProps) => {
-  const handleButtonClick = () => {
-    console.log("Button Clicked");
-  };
-
   return (
     <div className="relative w-full h-screen overflow-hidden ">
       <Image
         src={image}
         alt="Featured Image of the current media selected"
         fill={true}
-        className="w-full h-full object-cover object-center"
+        className="w-full h-full object-cover object-center opacity-50"
       />
       <div className="absolute w-full h-full z-[2] bg-background-movie flex items-end px-4 md:px-12 opacity-0 transition-opacity duration-500 ease-in-out hover:opacity-100">
         <div className="content-wrapper">
@@ -29,18 +25,9 @@ const FeaturedImage = ({ title, overview, image }: FeaturedImageProps) => {
             {overview}
           </p>
           <div className="flex items-center gap-3 mb-20">
-            <button
-              className="bg-color-secondary w-10 h-10 rounded-[50%] flex justify-center items-center transform transition duration-300 ease-in-out hover:scale-110"
-              onClick={handleButtonClick}
-            >
+            {/* <button className="bg-color-secondary w-10 h-10 rounded-[50%] flex justify-center items-center transform transition duration-300 ease-in-out hover:scale-110">
               <FaPlay />
-            </button>
-            <button
-              className="bg-color-secondary w-10 h-10 rounded-[50%] flex justify-center items-center transform transition duration-300 ease-in-out hover:scale-110"
-              onClick={handleButtonClick}
-            >
-              <FaPlus />
-            </button>
+            </button> */}
           </div>
         </div>
       </div>
